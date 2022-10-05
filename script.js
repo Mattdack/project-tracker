1558a2cba0a6900896d00997930c0ad658c8a16e
Matthew Dacanay <mattdack@uw.edu>
2022-10-05 12:31:37 -0700

    Added some of the initial HTML with bootstrap functionality

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 
-
+var interval = setInterval(function() {
+var timeEl = $('#date-time');
+var currentTime = moment().format("MMM Do, YYYY HH:mm:ss")
+timeEl.text(currentTime);
+}, 1000)
 
 
 $('#myModal').on('shown.bs.modal', function () {
